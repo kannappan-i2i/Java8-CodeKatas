10608874842fad44223296824562f4e142de6916
kannappan-i2i <kannappan.chidambaram@ideas2it.com>
2023-01-29 18:05:39 +0530

    Upgraded as maven project

---
+++ codekata/src/main/java/com/java/codekata/CodeKataApp.java
@@ -0,0 +1,13 @@
+package com.java.codekata;
+
+/**
+ * Hello world!
+ *
+ */
+public class CodeKataApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
